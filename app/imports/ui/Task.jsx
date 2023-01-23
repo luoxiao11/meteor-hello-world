d2fb92c789a8b7c5365906fc0df99f6d140948db
luoxiao11 <luoxiao.rf@gmail.com>
2023-01-23 11:26:28 -1000

    change1

--- app/imports/ui/Task.jsx
+++ app/imports/ui/Task.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
 export const Task = ({ task }) => {
+  console.log("ttt2")
   return <li>{task.text}</li>
-};
+s};
---
+++ app/imports/ui/TaskForm.jsx
@@ -0,0 +1,4 @@
+import { Mongo } from 'meteor/mongo';
+console.log("ttt3")
+
+export const TasksCollection = new Mongo.Collection('tasks');
